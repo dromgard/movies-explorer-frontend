df7f98882ca2cb2f8d3b624ad8d6ff3fe3e9baca
Sergei Korolev <dromgard@yandex.ru>
2022-12-31 20:43:20 +0300

    #2 верстка станицы 'О проекте'

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AboutProject() {
+  return (
+    <section className="about-project">
+      <h2 className="section-title">О проекте</h2>
+      <div className="about-project__info">
+        <p className="about-project__subtitle">Дипломный проект включал 5 этапов</p>
+        <p className="about-project__subtitle">На выполнение диплома ушло 5 недель</p>
+        <span className="section-text">Составление плана, работу над бэкендом, вёрстку,
+          добавление функциональности и финальные доработки.</span>
+        <span className="section-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно
+          было соблюдать, чтобы успешно защититься.</span>
+      </div>
+      <div className="about-project__plan">
+        <div className="about-project__plan-subtitle about-project__plan-subtitle_done">1 неделя</div>
+        <div className="about-project__plan-subtitle">4 недели</div>
+        <p className="about-project__plan-text">Back-end</p>
+        <p className="about-project__plan-text">Front-end</p>
+      </div>
+
+    </section>
+  );
+}
+
+export default AboutProject;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AboutProject from "../AboutProject/AboutProject";
+import Promo from "../Promo/Promo";
+
+function Main() {
+  return (
+    <main className="main">
+      <Promo />
+      <AboutProject />
+    </main>
+  );
+}
+
+export default Main;
+
